45f713b0c853862a5d4d8eb338350a3f1ef31a3e
Rishi <rishi@example.com>
2026-03-14 14:38:19 +0530

    remove db and gen files

--- gen.py
+++
@@ -1,8 +0,0 @@
-from py_vapid import Vapid
-
-v = Vapid()
-v.generate_keys()
-v.save_key("private_key.pem")
-v.save_public_key("public_key.pem")
-
-print("Done! Check private_key.pem and public_key.pem in your folder")
